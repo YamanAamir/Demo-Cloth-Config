5793dae824c6e81545fc5c52635fc2f3f4fa1756
yamanaamir <yamanaamir2004@gmail.com>
2026-06-12 22:35:14 +0500

    Update StudentDashBoard.jsx

--- src/Screens/StudentDashBoard.jsx
+++ src/Screens/StudentDashBoard.jsx
@@ -1006,7 +1006,7 @@ const StudentDashboard = ({ customizations, setCustomizations, setShowBackPopup 
                     </div>
                     <div className="flex flex-1 h-full">
                         <div className="flex-1">
-                            <div className="bg-white/50 backdrop-blur-sm rounded-2xl shadow-xl h-full flex flex-col border border-slate-200">
+                            <div className="bg-white/50 backdrop-blur-sm  h-full flex flex-col border border-slate-200">
                                 {/* <div className="flex items-center justify-between p-6 border-b border-slate-200">
                                     <div className="flex items-center space-x-3">
                                         <div className="w-10 h-10 bg-gradient-to-r from-green-600 to-green-600 rounded-xl flex items-center justify-center">
